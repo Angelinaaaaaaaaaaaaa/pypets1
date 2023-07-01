55c725ee99f4fd9fe2056c4d52ee2f6560bc45f9
Angelinaaaaaaaaaaaaa <138276474+Angelinaaaaaaaaaaaaa@users.noreply.github.com>
2023-07-01 11:28:53 +0300

    Create Info.cs

---
+++ Assets/Scripts/melochi/Info.cs
@@ -0,0 +1,14 @@
+public static class Memory
+{
+    public static float mVolue = 0.5f;
+    public static string name;
+    public static string nick;
+    public static int food;
+    public static int water;
+    public static double energy;
+    public static double hungry;
+    public static double health;
+    public static int id;
+    public static int coins;
+    public static int exp;
+}
